990affe3d3ed625df9de1bc3e0828cefbcc098ce
Ghazimoradi <ghazimoradi.amin@gmail.com>
2024-12-05 15:56:26 +0330

    Implementation Requesting Dangerous Permissions

---
+++ app/src/main/java/project/activity/MainActivity.java
@@ -0,0 +1,96 @@
+package project.activity;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.permissions.R;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Log.i(TAG, "permission : " + permission);
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            String[] permissions = {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            askUserForPermission(permissions, 100);
+            String[] permissions2 = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            };
+            askUserForPermission(permissions2, 101);
+        }
+    }
+
+    private void askUserForPermission(String[] permissions, int requestCode) {
+        ActivityCompat.requestPermissions(this, permissions, requestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Write Access to SDCARD granted");
+                createAppDir();
+            } else {
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission Required")
+                        .setMessage("Write Access to SDCARD required for app")
+                        .setPositiveButton("Ask me again", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //askUserForPermission();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        } else if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Read Access to SDCARD granted");
+                createAppDir();
+            } else {
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission Required")
+                        .setMessage("Read Access to SDCARD required for app")
+                        .setPositiveButton("Ask me again", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //askUserForPermission();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void createAppDir() {
+        String dirName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/uncox";
+        File file = new File(dirName);
+        boolean wasCreated = file.mkdirs();
+        if (wasCreated) {
+            Log.i(TAG, "Yes");
+        } else {
+            Log.i(TAG, "No");
+        }
+    }
+}
---
+++ app/src/main/java/project/main/G.java
@@ -0,0 +1,10 @@
+package project.main;
+
+import android.app.Application;
+
+public class G extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
